328a7f196e9644fe2079089c7043c1240ca347f3
sarower ahmmed <studentttech@gail.com>
2019-09-01 16:57:05 -0400

    version 1

---
+++ SundayAfterNoonAutomation/src/main/java/com/genericode/GCRLoginSunday.java
@@ -0,0 +1,71 @@
+package com.genericode;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class GCRLoginSunday {
+static WebDriver driver;
+
+@BeforeTest
+public void setup() {
+//Test 01= User need to open a browser
+
+System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+driver = new ChromeDriver();// upcasting
+
+driver.manage().window().maximize(); //mazimize window
+
+
+}
+
+
+
+@Test
+public void gcrLogin() {
+
+
+//test 02 =Go to GCR shop
+
+driver.get("http://www.gcrit.com/build3/");
+
+//Click my account
+MasterPageFactory	pf = PageFactory.initElements(driver, MasterPageFactory.class);
+
+pf.getMyAccount().get(0).click();
+
+//put email
+
+pf.getGcrEmail().get(0).sendKeys("sarowerny@gmail.com");
+
+//put password
+
+pf.getGcrPassword().get(0).sendKeys("student");
+
+//click sign in button
+
+
+pf.getGcrSignin().get(0).click();
+
+//login will be successful
+
+Assert.assertTrue(pf.getGcrLogout().get(0).isDisplayed());
+
+
+System.out.println("Test done");
+
+
+
+}
+@AfterTest
+public void teardown() {
+driver.quit();
+}
+
+
+
+}
